12c5abbdc9dd5c7f1bf1acd05427e2ad42b4a89d
Maximilian Coghlan <max.coghlan@icloud.com>
2021-01-19 20:07:39 +0000

    mini blog

--- src/util/projects.ts
+++ src/util/projects.ts
@@ -2,6 +2,7 @@
 import modernCalligraphy from "./projectData/modernCalligraphy";
 import bellPepper from "./projectData/bellPepper";
 import humblr from "./projectData/humblr";
+import miniBlog from "./projectData/miniBlog";
 
 // ARRAY OF ALL PROJECTS
-export const PROJECTS = [modernCalligraphy, bellPepper, humblr];
+export const PROJECTS = [modernCalligraphy, bellPepper, humblr, miniBlog];
