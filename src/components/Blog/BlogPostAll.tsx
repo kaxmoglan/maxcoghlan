58b467b21ba25861eba448a46030a69d9bc57084
Maximilian Coghlan <max.coghlan@icloud.com>
2021-01-23 21:16:21 +0000

    loading functionality. Fixed useEffect call when leaving blog

--- src/components/Blog/BlogPostAll.tsx
+++ src/components/Blog/BlogPostAll.tsx
@@ -1,6 +1,46 @@
 import React from "react";
 
-export const BlogPostAll: React.FC = () => {
+interface IALLPOSTS {
+  type_of: null | string;
+  id: null | number;
+  title: null | string;
+  description: null | string;
+  readable_publish_date: null | string;
+  slug: null | string;
+  path: null | string;
+  url: null | string;
+  comments_count: null | number;
+  public_reactions_count: null | number;
+  collection_id: null | number;
+  published_timestamp: null | string;
+  positive_reactions_count: null | number;
+  cover_image: string | null;
+  social_image: null | string;
+  canonical_url: null | string;
+  created_at: null | string;
+  edited_at: null | string;
+  crossposted_at: null | string;
+  published_at: null | string;
+  last_comment_at: null | string;
+  tag_list: null | string[];
+  tags: null | string[];
+  user: {
+    name: null | string;
+    username: null | string;
+    twitter_username: null | string;
+    github_username: null | string;
+    website_url: null | string;
+    profile_image: null | string;
+    profile_image_90: null | string;
+  };
+}
+
+interface Props {
+  allPosts: Array<IALLPOSTS>;
+}
+
+export const BlogPostAll: React.FC<Props> = (props) => {
+  const { allPosts } = props;
   return (
     <>
       <h1>ALL BLOG POSTS</h1>
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 
 import { Link, useParams } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
+import axios from "axios";
+import Loader from "react-loader-spinner";
 
 import { BlogHeader } from "../components/Blog/BlogHeader";
 import { BlogAbout } from "../components/Blog/BlogAbout";
 import { BlogLatestPosts } from "../components/Blog/BlogLatestPosts";
+import { BlogPostAll } from "../components/Blog/BlogPostAll";
 
 interface Props {
   setTheme: Function;
@@ -13,24 +16,60 @@ interface Props {
   setShowSocial: Function;
 }
 
+const api = axios.create({
+  baseURL: "https://dev.to/api/",
+});
+
 export const Blog: React.FC<Props> = (props) => {
   const { setTheme, setShowNav, setShowSocial } = props;
 
   const { id }: any = useParams();
   const [loading, setLoading] = useState(true);
-  const [allPosts, setAllPosts] = useState();
-  const [post, setPost] = useState();
+  const [allPosts, setAllPosts] = useState([]);
+  const [post, setPost] = useState({});
   const [showAbout, setShowAbout] = useState(true);
 
-  // SET GLOBAL STATE ON LOAD
+  // GET REQUESTS
+  const getAllPosts = () => {
+    setLoading(true);
+    api
+      .get("/articles?username=jameswallis&state=all")
+      .then((res) => {
+        setAllPosts(res.data);
+        setTimeout(() => setLoading(false), 2000);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getSinglePost = () => {
+    setLoading(true);
+    api
+      .get(`/articles/${id}`)
+      .then((res) => {
+        setPost(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // GLOBAL STATE
   useEffect(() => {
     setTheme("light");
     setShowNav(false);
     setShowSocial(false);
 
-    // eslint-disable-next-line
-  }, []);
+    getAllPosts();
+  }, [setTheme, setShowNav, setShowSocial]);
 
+  // GET SINGLE POSTS
+  useEffect(() => {
+    if (id !== "home" && id !== undefined) {
+      getSinglePost();
+    }
+
+    console.log(id);
+  }, [id]);
+
+  // HANDLERS
   const handleClick = () => {
     setShowAbout(!showAbout);
   };
@@ -61,6 +100,18 @@ export const Blog: React.FC<Props> = (props) => {
         </div>
         <div className="right-column">
           <p onClick={() => handleClick()}>CLICK ME</p>
+          {loading && (
+            <div className="spinner__container">
+              <Loader
+                type="TailSpin"
+                color="#000"
+                height={50}
+                width={50}
+                className="spinner"
+              />
+            </div>
+          )}
+          {!loading && <BlogPostAll allPosts={allPosts} />}
         </div>
       </div>
     </div>
